72d0430ac4160e8a3b36402bcfccac02b2e36f36
tiger <764427188@qq.com>
2019-08-09 21:39:40 +0800

    fixed:all

--- bin/mock-servers.js
+++ bin/mock-servers.js
@@ -1,14 +1,28 @@
 #! /usr/bin/env node
 
 const path = require("path");
+const Compiler = require("../lib/Compiler.js");
+const resolveConfig = require("../lib/resolveConfig.js");
+const resolvePrefix = require("../lib/resolvePrefix.js");
+
 let config = null;
+
+let configPath = resolvePrefix(resolveConfig("path"));
+// console.log("configPath", configPath);
 try {
-  config = require(path.resolve("mock.server.js"));
+  if (configPath) {
+    config = require(path.resolve(process.cwd(), configPath));
+  } else {
+    config = require(path.resolve(process.cwd(), "mock.config.js"));
+  }
+  // console.log("读取config：", config);
 } catch (e) {
-  throw Error("未找到mockServer配置文件，请确认根目录有mock.server.js配置文件");
+  throw Error(
+    "未找到mockServer配置文件，请确认根目录有mock.config.js配置文件!",
+    e
+  );
 }
 
-let Compiler = require("../lib/Compiler.js");
 let compiler = new Compiler(config);
-// // 标识运行编译
-compiler.run();
+// 标识 运行编译
+compiler.run(configPath);
